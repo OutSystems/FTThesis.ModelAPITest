c4e1c42415e0a6a91241e06fdb9b857cc3ebaef4
Bárbara Lopes <barbara.lopes@outsystems.com>
2021-05-06 15:03:50 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using OutSystems.Model.UI.Web;
+
+namespace ModelAPITest
+{
+    class Program
+    {
+
+        private const string TWSimpleScreensESpacePath = @"..\..\..\SimpleScreensTW.oml";
+
+        private const string NRSimpleScreensESpacePath = @"..\..\..\SimpleScreensNR.oml";
+
+        static void Main(string[] args)
+        {
+            //if (args.Length != 1)
+            //{
+            //  Console.WriteLine("Usage: <File.oml>");
+            //return;
+            //}
+
+            var eSpacePath = Console.ReadLine();
+
+            //var eSpacePath = args[0];
+            if (!File.Exists(eSpacePath))
+            {
+                Console.WriteLine($"File {eSpacePath} not found");
+                return;
+            }
+
+            var modelServices = OutSystems.ModelAPILoader.Loader.ModelServicesInstance;
+
+            var module = modelServices.LoadESpace(eSpacePath);
+
+            var listScreens = module.GetAllDescendantsOfType<IWebScreen>();
+
+            Console.WriteLine("Screens:");
+
+            foreach (IWebScreen screen in listScreens)
+            {
+                Console.WriteLine(screen.Name);
+            }
+
+            var listwebblocks = module.GetAllDescendantsOfType<IWebBlock>();
+
+            Console.WriteLine("\nWebBlocks:");
+
+            foreach (IWebBlock block in listwebblocks)
+            {
+                Console.WriteLine(block.Name);
+            }
+          
+        }
+
+        
+    }
+}
